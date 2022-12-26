a121c8f7613f2ba7b11d41ae91e6808c6c3da1a9
Adrián José Martínez Navarro <admartnav@gmail.com>
2022-12-26 18:37:43 +0100

    First utils and how to use sample file. Placeholders for submissions.

---
+++ utils.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import datetime
+
+def load_data():
+    return pd.read_csv("data/train.csv")
+
+def generate_submission(labels, method, notes = ""):
+    test = pd.read_csv("data/test.csv")
+    df = pd.DataFrame(test.PassengerId) 
+
+    df["Transported"] = ["False" if p == 0 else "True" for p in labels]
+
+    name = "submissions/" + method + "/" + notes + "_" + str(datetime.datetime.now()).replace("-", "_") + ".csv"
+    df.to_csv(name.replace(" ", "_"), header = ['PassengerId', 'Transported'], index = False)
