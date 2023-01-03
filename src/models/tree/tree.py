8dcea56b5e6fc5a9491274cb89c95e92f7efee09
Ricardo <richardignacio888@gmail.com>
2023-01-03 21:30:15 +0100

    Primeras pruebas

--- src/models/tree/tree.py
+++ src/models/tree/tree.py
@@ -1,13 +1,16 @@
 import sys
-sys.path.insert(1, '.\\src')
-import utils
+sys.path.insert(1, './src')
+from utils import *
+import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_validate
 
-train_raw = utils.load_data()
+
+train_raw = load_train()
 train_x=train_raw.drop(['Transported', 'PassengerID'], axis=1)
 train_y = train_raw.Transported
 
-cv = cross_validate(estimator = DecisionTreeClassifier(), x = train_x, y = train_y, cv = 10, n_jobs = -1, verbose = 5)
+cv = cross_validate(estimator = DecisionTreeClassifier(), x = train_x, y = train_y,
+                    cv = 10, n_jobs = -1, verbose = 5)
 print(np.mean.cv(['test_score']))
