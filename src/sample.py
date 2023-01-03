3b148fbaef607996cf32cc1d0c30ad4c0582809b
Adrián José Martínez Navarro <admartnav@gmail.com>
2023-01-03 18:35:17 +0100

    Partial implementation OneHotEcoding function

--- src/sample.py
+++ src/sample.py
@@ -1,9 +1,4 @@
-import utils
+from utils import *
 import numpy as np
 
-preds = np.random.randint(low = 0, high = 2, size = 4277)
-utils.generate_submission(labels = preds, method = "svm", notes = "placeholder")
-utils.generate_submission(labels = preds, method = "bayes", notes = "placeholder")
-utils.generate_submission(labels = preds, method = "log", notes = "placeholder")
-utils.generate_submission(labels = preds, method = "tree", notes = "placeholder")
-utils.generate_submission(labels = preds, method = "knn", notes = "placeholder")
+one_hot_encode(None, None)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,21 @@
 import pandas as pd
 import datetime
+from sklearn.preprocessing import OneHotEncoder
+import pickle
 
 def load_data():
     return pd.read_csv("data/train_pr.csv")
+
+def one_hot_encode(df):
+    filenames = ["data/pickles/one_hot/oh_destination.pck", "data/pickles/one_hot/oh_home_planet.pck", 
+                 "data/pickles/one_hot/oh_cryosleep.pck", "data/pickles/one_hot/oh_vip.pck", 
+                 "data/pickles/one_hot/oh_cabin_deck.pck", "data/pickles/one_hot/oh_cabin_side.pck"]
+    categorical_variables = ["Destination", "HomePlanet", "CryoSleep", "VIP", "Cabin_deck", "Cabin_side"]
+    for filename, var in zip(filenames, categorical_variables):
+        with open(file = filename, mode = "rb") as f:
+            oh_e = pickle.load(file = f)
+
+
 
 def generate_submission(labels, method, notes = ""):
     test = pd.read_csv("data/test_pr.csv")
