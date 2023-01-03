15fee6ad3fea4be8842745971e78ccae61183a07
Adrián José Martínez Navarro <admartnav@gmail.com>
2023-01-03 21:41:03 +0100

    First test with DecisionTreeClassifier and 10-Fold cross validation. 73% accuracy

--- src/models/tree/tree.py
+++ src/models/tree/tree.py
@@ -6,11 +6,9 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_validate
 
-
 train_raw = load_train()
-train_x=train_raw.drop(['Transported', 'PassengerID'], axis=1)
+train_x = one_hot_encode(df = train_raw.drop(['Transported', 'PassengerId'], axis=1))
 train_y = train_raw.Transported
 
-cv = cross_validate(estimator = DecisionTreeClassifier(), x = train_x, y = train_y,
-                    cv = 10, n_jobs = -1, verbose = 5)
-print(np.mean.cv(['test_score']))
+cv = cross_validate(estimator = DecisionTreeClassifier(), X = train_x, y = train_y, cv = 10, n_jobs = -1, verbose = 5)
+print(np.mean(cv['test_score']))
