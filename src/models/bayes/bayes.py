877e49d16981775215fd697538c66c2d388b1abc
Javier Gamero <javigamero4@gmail.com>
2023-01-04 11:12:51 +0100

    Watching the path error

---
+++ src/models/bayes/bayes.py
@@ -0,0 +1,8 @@
+import os
+import sys
+MODELS = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+SRC = os.path.abspath(os.path.join(MODELS, os.pardir))
+sys.path.append(SRC)
+
+import utils 
+import numpy as np
