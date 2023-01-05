617adefe92df315fe205ea3df4d999aa18f46d85
eusebio <eusebio.alsoliman@gmail.com>
2023-01-05 15:08:58 +0100

    Prototipo de normalizer 3

--- src/models/log/log.py
+++ src/models/log/log.py
@@ -1,24 +1,37 @@
+import os
 import sys
-sys.path.insert(1, './src')
-from utils import *
+
+MODELS = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+SRC = os.path.abspath(os.path.join(MODELS, os.pardir))
+sys.path.append(SRC)
+
+import utils
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_validate
 
-train_raw = load_train()
-train_X = one_hot_encode(df = train_raw.drop(["Transported", "PassengerId"], axis = 1))
+train_raw = utils.load_train()
+train_raw["SM_FC"] = train_raw["ShoppingMall"] + train_raw["FoodCourt"]
+train_raw["VD_SP"] = train_raw["Spa"] + train_raw["VRDeck"]
+train_raw = train_raw.drop(["ShoppingMall", "FoodCourt", "Spa", "VRDeck"], axis=1)
+# train_raw = train_raw.drop(["Cabin_deck", "Cabin_side"], axis=1)
+print(train_raw)
+train_X = utils.one_hot_encode(df = train_raw.drop(["Transported", "PassengerId"], axis = 1))
+train_X = utils.normalize(df = train_X)
 train_y = train_raw.Transported
 
-cv = cross_validate(estimator = LogisticRegression(random_state = 1234), X = train_X, y = train_y, cv = 10, n_jobs = -1, verbose = 5)
+cv = cross_validate(estimator = LogisticRegression(random_state = 1234, max_iter=5000), X = train_X, y = train_y, cv = 10, n_jobs = -1, verbose = 5)
 print("Cross-validation test score: ", np.mean(cv["test_score"]))
 
-test_raw = load_test()
-test = one_hot_encode(df = test_raw.drop(["PassengerId"], axis = 1))
+# --------------- CREAR PREDICCIONES PARA EL TEST ---------------------------------
 
-print("Training LogisticRegression...")
-log = LogisticRegression(verbose=True, random_state=1234).fit(X = train_X, y = train_y)
-print("Making predictions...")
-pred_labels = log.predict(X = test)
-print(pred_labels)
+# test_raw = utils.load_test()
+# test = utils.one_hot_encode(df = test_raw.drop(["PassengerId"], axis = 1))
 
-generate_submission(labels = pred_labels, method = "log", notes = "default_parameters")
+# print("Training LogisticRegression...")
+# log = LogisticRegression(verbose=True, random_state=1234, max_iter = 5000).fit(X = train_X, y = train_y)
+# print("Making predictions...")
+# pred_labels = log.predict(X = test)
+# print(pred_labels)
+
+# utils.generate_submission(labels = pred_labels, method = "log", notes = "default_parameters")
