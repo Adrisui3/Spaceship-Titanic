0de02e61923af7b85bc7e8fffaca3d8e5a024bc6
eusebio <eusebio.alsoliman@gmail.com>
2023-01-05 18:58:54 +0100

    Añadido el grid search y mejora de accuracy con max_iter = 50

--- src/models/log/log_grid_search.py
+++ src/models/log/log_grid_search.py
@@ -21,19 +21,36 @@ train_X = utils.one_hot_encode(df = train_raw.drop(["Transported", "PassengerId"
 # train_X = utils.normalize(df = train_X)
 train_y = train_raw.Transported
 
-cv = cross_validate(estimator = LogisticRegression(random_state = 1234, max_iter=50), X = train_X, y = train_y, cv = 10, n_jobs = -1, verbose = 5)
-print("Cross-validation test score: ", np.mean(cv["test_score"]))
+log = LogisticRegression(random_state = 1234)
+
+# iter_range = list(range(1, 100))
+iter_range = list(range(100,200))
+
+# Con iter_range entre 100 y 200, tenemos que el mejor es 129 y 0.78868 de accuracy
+
+range_tol = [1e-4, 1e-3, 1e-2, 1e-1, 1, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9]
+range_C = list(range(1,10))
+
+pgrid = dict(max_iter=iter_range)
+
+grid = GS_CV(log, pgrid, scoring='accuracy', n_jobs = -1, cv =10, return_train_score=True)
+
+grid_search = grid.fit(train_X,train_y)
+
+print(grid_search.best_params_)
+
+print(grid_search.best_score_)
+
+# cv = cross_validate(estimator = LogisticRegression(random_state = 1234, max_iter=5000), X = train_X, y = train_y, cv = 10, n_jobs = -1, verbose = 5)
+# print("Cross-validation test score: ", np.mean(cv["test_score"]))
 
 # --------------- CREAR PREDICCIONES PARA EL TEST ---------------------------------
 
 # test_raw = utils.load_test()
-# test_raw["SM_FC"] = test_raw["ShoppingMall"] + test_raw["FoodCourt"]
-# test_raw["VD_SP"] = test_raw["Spa"] + test_raw["VRDeck"]
-# test_raw = test_raw.drop(["ShoppingMall", "FoodCourt", "Spa", "VRDeck"], axis=1)
 # test = utils.one_hot_encode(df = test_raw.drop(["PassengerId"], axis = 1))
 
 # print("Training LogisticRegression...")
-# log = LogisticRegression(verbose=True, random_state=1234, max_iter = 50).fit(X = train_X, y = train_y)
+# log = LogisticRegression(verbose=True, random_state=1234, max_iter = 5000).fit(X = train_X, y = train_y)
 # print("Making predictions...")
 # pred_labels = log.predict(X = test)
 # print(pred_labels)
--- src/models/log/log_max_iter_50_SMFC_VDSP.py
+++ src/models/log/log_max_iter_50_SMFC_VDSP.py
@@ -26,16 +26,16 @@ print("Cross-validation test score: ", np.mean(cv["test_score"]))
 
 # --------------- CREAR PREDICCIONES PARA EL TEST ---------------------------------
 
-# test_raw = utils.load_test()
-# test_raw["SM_FC"] = test_raw["ShoppingMall"] + test_raw["FoodCourt"]
-# test_raw["VD_SP"] = test_raw["Spa"] + test_raw["VRDeck"]
-# test_raw = test_raw.drop(["ShoppingMall", "FoodCourt", "Spa", "VRDeck"], axis=1)
-# test = utils.one_hot_encode(df = test_raw.drop(["PassengerId"], axis = 1))
+test_raw = utils.load_test()
+test_raw["SM_FC"] = test_raw["ShoppingMall"] + test_raw["FoodCourt"]
+test_raw["VD_SP"] = test_raw["Spa"] + test_raw["VRDeck"]
+test_raw = test_raw.drop(["ShoppingMall", "FoodCourt", "Spa", "VRDeck"], axis=1)
+test = utils.one_hot_encode(df = test_raw.drop(["PassengerId"], axis = 1))
 
-# print("Training LogisticRegression...")
-# log = LogisticRegression(verbose=True, random_state=1234, max_iter = 50).fit(X = train_X, y = train_y)
-# print("Making predictions...")
-# pred_labels = log.predict(X = test)
-# print(pred_labels)
+print("Training LogisticRegression...")
+log = LogisticRegression(verbose=True, random_state=1234, max_iter = 50).fit(X = train_X, y = train_y)
+print("Making predictions...")
+pred_labels = log.predict(X = test)
+print(pred_labels)
 
-# utils.generate_submission(labels = pred_labels, method = "log", notes = "default_parameters")
+utils.generate_submission(labels = pred_labels, method = "log", notes = "default_parameters")
