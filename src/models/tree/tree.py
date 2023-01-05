a6bfbf7533341ff81ef1b7b16574a4de196d2540
Ricardo <richardignacio888@gmail.com>
2023-01-05 16:51:37 +0100

    Primeras pruebas

--- src/models/tree/tree.py
+++ src/models/tree/tree.py
@@ -1,13 +1,17 @@
+import os
 import sys
-sys.path.insert(1, './src')
-from utils import *
+MODELS = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+SRC = os.path.abspath(os.path.join(MODELS, os.pardir))
+sys.path.append(SRC)
+import utils
 import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_validate
 
-train_raw = load_train()
-train_x = one_hot_encode(df = train_raw.drop(['Transported', 'PassengerId'], axis=1))
+
+train_raw = utils.load_train()
+train_x = utils.one_hot_encode(df = train_raw.drop(['Transported', 'PassengerId'], axis=1))
 train_y = train_raw.Transported
 
 cv = cross_validate(estimator = DecisionTreeClassifier(), X = train_x, y = train_y, cv = 10, n_jobs = -1, verbose = 5)
