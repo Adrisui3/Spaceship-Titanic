d6c14488bfa40dfb75d44eb951459d09e680fcf2
Javier Gamero <javigamero4@gmail.com>
2023-01-10 10:01:11 +0100

    Mixed GaussianNB and MultinomialNB manually

--- src/models/bayes/Mix_GNB_MNB_Manual.py
+++ src/models/bayes/Mix_GNB_MNB_Manual.py
@@ -14,6 +14,7 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.preprocessing import Normalizer
 # Training data load and OneHotEncoder
 print('--- LOADING DATA ---')
+# TRAIN
 train_raw = utils.load_train()
 train_X = utils.one_hot_encode(train_raw.drop(['Transported', 'PassengerId'], 
                                               axis = 1))
@@ -23,19 +24,56 @@ train_y = train_raw['Transported'].copy()
 num_features = np.arange(0,6)
 cat_features = np.arange(6,20)
 
+#TEST
+test_raw = utils.load_test()
+test = utils.one_hot_encode(test_raw.drop(['PassengerId'], axis = 1))
 
-print('--- NUMERICAL DATA ---')
+
+print('\n--- NUMERICAL DATA ---')
 train_Xnum = train_X.iloc[:, num_features].copy()
 train_Xnum_norm =  Normalizer().fit_transform(train_Xnum)
 
-gnb = GaussianNB().fit(X = train_Xnum_norm, y = train_y)
-gaussian_log_proba = gnb.predict_log_proba(train_Xnum_norm)
-print(gaussian_log_proba)
+test_num = test.iloc[:, num_features].copy()
+test_num_norm = Normalizer().fit_transform(test_num)
 
-print('--- Categorical data ---')
+gnb = GaussianNB().fit(X = train_Xnum_norm, y = train_y)
+gaussian_jll = gnb._joint_log_likelihood(test_num_norm)
+# print('\n', gaussian_jll)
+# print(gnb.class_prior_)
+
+
+print('\n--- CATEGORICAL DATA ---')
 train_Xcat = train_X.iloc[:, cat_features].copy()
 
+test_cat = test.iloc[:, cat_features].copy()
+
 mnb = MultinomialNB().fit(X = train_Xcat, y = train_y)
-multinomial_log_proba = mnb.predict_log_proba(train_Xcat)
-print(multinomial_log_proba)
-print('\n', Normalizer().fit_transform(multinomial_log_proba))
+multinomial_jll = mnb._joint_log_likelihood(test_cat) # not normalized
+log_prior = mnb.class_log_prior_
+# print(np.exp(log_prior))
+# print('\n', multinomial_jll)
+
+
+print('\n--- JOINING PROBABILITIES ---')
+jlls = []
+jlls.append(gaussian_jll)
+jlls.append(multinomial_jll)
+jlls = np.hstack([jlls])
+
+jll = jlls.sum(axis = 0)
+proba = jll - log_prior # log probabilities
+# print(proba)
+
+#standarise data
+num = np.exp(jll) 
+div = np.sum(num, axis = 1, keepdims = True) 
+proba_norm = num/div
+
+pred_labels = np.bool_(np.argmax(proba_norm, axis = 1))
+print(pred_labels)
+
+utils.generate_submission(
+    labels = pred_labels, 
+    method = 'bayes', 
+    notes = 'Mixed_Gaussian_Multinomial_NB_MANUAL'
+)
