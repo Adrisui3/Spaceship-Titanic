240e4af9d1b3d8d5a4a754ce60071448e564ba5e
Marcos Esquivel Gonzalez <marcosesquivelgonzalez@MacBook-Pro-de-Marcos.local>
2023-01-13 12:27:07 +0100

    Implementacion de imputacion knn usando OrdinalEncoders y probandolo

--- src/KnnImputation.py
+++ src/KnnImputation.py
@@ -7,17 +7,108 @@ Created on Fri Jan 13 00:27:16 2023
 """
 import os
 import pandas as pd
+import numpy as np
 from sklearn.impute import KNNImputer
-
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 train_raw  = pd.read_csv(ROOT + '/data/train_pr_to_KnnImp.csv')
+
+Transported = train_raw.Transported
+passID = train_raw[['PassengerId']]
+train_raw.drop(['PassengerId','Name','Cabin_num'],axis=1,inplace=True)
+
+
 test_raw = pd.read_csv(ROOT + '/data/test_pr_to_KnnImp.csv')
+
+passIdtest = test_raw[['PassengerId']]
+test_raw.drop(['PassengerId','Name','Cabin_num'],axis=1,inplace=True)
+
+#------------ Codifico sin passenger Id ------------------------------------------
+train_raw.info()
+
+test_raw.info()
+
+train_encoded = train_raw.copy()
+test_encoded = test_raw.copy()
+
+cat_columns = test_raw.select_dtypes(include=['object']).columns#Así no cojo transported
+
+enc = OrdinalEncoder(handle_unknown='use_encoded_value',unknown_value=np.nan)
+enc.fit(train_raw[cat_columns])
+
+train_encoded[cat_columns] = enc.transform(train_raw[cat_columns])
+test_encoded[cat_columns] = enc.transform(test_raw[cat_columns])
+
+#-----------------Standarizo e imputo SOLO con train---------------------------------------------------------
+colnames = train_encoded.columns
+
+
+StandScal = StandardScaler()
+
+train_EncScal_array = StandScal.fit_transform(train_encoded)
+train_EncScal = pd.DataFrame(train_EncScal_array,columns=colnames)
 
 imputer = KNNImputer(n_neighbors=30)
 
-imputer.fit(train_raw)
+train_EncScalImp = imputer.fit_transform(train_EncScal)
+train_EncScalImp = pd.DataFrame(train_EncScalImp,columns=colnames)
 
-train_raw_knnI = imputer.transform(train_raw)
-test_raw_knn =imputer.transform(test_raw)
+#---------------------Quito estandarizacion y codificación-----------------------------
+train_EncImp_array = StandScal.inverse_transform(train_EncScalImp)
+train_EncImp = pd.DataFrame(train_EncImp_array,columns=colnames)
+
+
+train_Imp = train_EncImp.copy()
+train_Imp[cat_columns] = enc.inverse_transform(train_EncImp[cat_columns])
+
+#---------------------------Pasamos ahora a estandarizar e imputar para test------------------------------------------
+
+#Cogemos los datos de train imputados y codificados y le quitamos transported_
+train_encoded_forTst = train_EncImp.drop(['Transported'],axis=1)
+colnames_tst = train_encoded_forTst.columns
+
+StandScal = StandardScaler()
+
+train_EncScal_array_forTst = StandScal.fit_transform(train_encoded_forTst)
+train_EncScal_forTst = pd.DataFrame(train_EncScal_array_forTst,columns=colnames_tst)
+
+test_EncScal_array = StandScal.transform(test_encoded)
+test_EncScal = pd.DataFrame(test_EncScal_array,columns = colnames_tst)
+
+imputer_forTst= KNNImputer(n_neighbors=30).fit(train_EncScal_forTst)
+
+test_EncScalImp_array = imputer_forTst.transform(test_EncScal)
+test_EncScalImp = pd.DataFrame(test_EncScalImp_array, columns=colnames_tst)
+
+#---------------------------Quito estandarización y codificación a test-------------------------- 
+test_EncImp_array = StandScal.inverse_transform(test_EncScalImp)
+test_EncImp = pd.DataFrame(test_EncImp_array,columns=colnames_tst)
+
+test_Imp = test_EncImp.copy()
+test_Imp[cat_columns] = enc.inverse_transform(test_EncImp[cat_columns])
+
+#---------------Añado PassengerId a los dos dataframes resultantes---------------------------
+
+train_Imp[['PassengerId']] = passID
+test_Imp[['PassengerId']] = passIdtest
+
+#Reordeno columnas
+colnames = train_Imp.columns.tolist()
+train_Imp = train_Imp[['PassengerId']+colnames[:-1]]
+
+colnames = test_Imp.columns.tolist()
+test_Imp = test_Imp[['PassengerId']+colnames[:-1]]
+
+#Pongo como buleanos transported
+train_Imp['Transported'] = train_Imp['Transported'].astype('bool')
+
+#--------------------------Exporto en csv los imputados-------------------
+
+train_Imp.to_csv(ROOT + '/data/train_pr_KnnImputed.csv',index=False)
+test_Imp.to_csv(ROOT + '/data/test_pr_KnnImputed.csv',index=False)
+
+
+
--- src/models/ensemble/ensemble_knn.py
+++ src/models/ensemble/ensemble_knn.py
@@ -16,7 +16,7 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import Normalizer
 
 
-train_raw = utils.load_train()
+train_raw = pd.read_csv('/Users/marcosesquivelgonzalez/Desktop/Master C.Datos/Projects/MDatos_PreprocClasif/Spaceship-Titanic/data/train_pr_KnnImputed.csv')
 train_raw = utils.merge_numerical(train_raw)
 train_X = utils.one_hot_encode(df = train_raw.drop(["Transported", "PassengerId"], axis = 1))
 
@@ -27,15 +27,15 @@ train_X_RobScaled = pd.DataFrame(df_array,columns=colnames)
 
 train_y = train_raw.Transported
 
-params = {"n_neighbors":39, "p":2}
+params = {"n_neighbors":53, "p":2}
 max_features = math.sqrt(train_X.shape[0]) / train_X.shape[0]
-gsen = ensemble.BaggingClassifier(weak_estimator = KNeighborsClassifier(), n_estimators = 90000, estimator_params = params, verbose = True)
+gsen = ensemble.BaggingClassifier(weak_estimator = KNeighborsClassifier(), n_estimators = 5000, estimator_params = params, verbose = True)
 gsen.fit(X = train_X_RobScaled, y = train_y)
 train_preds = gsen.predict(X = train_X_RobScaled)
 print("Mean OOB accuracy:", gsen.get_mean_oob_accuracy())
 print("Train score: ", accuracy_score(train_y, train_preds))
 
-test_raw = utils.load_test()
+test_raw = pd.read_csv('/Users/marcosesquivelgonzalez/Desktop/Master C.Datos/Projects/MDatos_PreprocClasif/Spaceship-Titanic/data/test_pr_KnnImputed.csv')
 test_raw = utils.merge_numerical(test_raw)
 test = utils.one_hot_encode(df = test_raw.drop(["PassengerId"], axis = 1))
 
@@ -44,4 +44,4 @@ test_scaled_array = RobScal.transform(test)
 test_scaled = pd.DataFrame(test_scaled_array,columns=colnames)
 
 pred_labels = gsen.predict(X = test_scaled)
-utils.generate_submission(labels = pred_labels, method = "ensemble", notes = "knn_euc_k39_merged_robscal_90000estimators")
+utils.generate_submission(labels = pred_labels, method = "ensemble", notes = "knn_euc_k52_merged_robscal_KnnImp_5000estimators")
