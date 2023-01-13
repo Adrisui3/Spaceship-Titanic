464435ceb2c5d84c5fd6d9329ff782e8f9ffe3c3
Marcos Esquivel Gonzalez <marcosesquivelgonzalez@MacBook-Pro-de-Marcos.local>
2023-01-14 00:40:21 +0100

    Resultados de knn usando la imputacion por knn

---
+++ src/models/knn/ImprovedKnn.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 19:47:31 2023
+
+@author: marcosesquivelgonzalez
+
+Se intenta implementar un nuevo knn basado en el paper:
+    An Improved kNN Based on Class Contribution and Feature Weighting
+    -HUANG Jie1,2, WEI Yongqing3,*,YI Jing2,4 and LIU Mengdi1,
+"""
+
