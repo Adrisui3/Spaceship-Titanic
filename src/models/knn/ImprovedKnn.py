8fc733da662a98240d56e02096fe96ba311cc3a9
Marcos Esquivel Gonzalez <marcosesquivelgonzalez@MacBook-Pro-de-Marcos.local>
2023-01-14 14:38:13 +0100

    implementando improved knn

--- src/models/knn/ImprovedKnn.py
+++ src/models/knn/ImprovedKnn.py
@@ -10,3 +10,40 @@ Se intenta implementar un nuevo knn basado en el paper:
     -HUANG Jie1,2, WEI Yongqing3,*,YI Jing2,4 and LIU Mengdi1,
 """
 
+import os
+import sys
+MODELS = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+SRC = os.path.abspath(os.path.join(MODELS, os.pardir))
+sys.path.append(SRC)
+
+import utils
+from sklearn.preprocessing import RobustScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors._base import KNeighborsMixin
+import numpy as np
+import pandas as pd
+
+train_raw = utils.load_train_KnnImp()
+train_raw = utils.merge_numerical(train_raw) 
+    
+train_X = utils.one_hot_encode(df = train_raw.drop(["Transported", "PassengerId"], axis = 1))
+
+RobScal = RobustScaler()
+df_array = RobScal.fit_transform(train_X)
+train_X_RobScal = pd.DataFrame(df_array,columns=train_X.columns)
+    
+    
+def improved_knn(n_neighbors=30,weights,train_data,test_data):
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
