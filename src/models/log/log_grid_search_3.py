ea1ac414c1c4aeef41ec8f7a45e591781dd7d622
eusebio <eusebio.alsoliman@gmail.com>
2023-01-15 12:03:09 +0100

    Actualización probando los datasets con imputación de NAs con k-NN

--- src/models/log/log_grid_search_3.py
+++ src/models/log/log_grid_search_3.py
@@ -22,6 +22,8 @@ train_X_cat = train_X.drop(["Age", "RoomService", "SM_FC", "VD_SP"], axis = 1)
 train_X_num = utils.normalizer_oh(train_X_num, merged=True, nra=True, onlynum=True)
 train_X = pd.concat([train_X_cat.reset_index(drop=True), train_X_num], axis=1)
 
+# train_X = train_X.drop(["VIP_1.0", "Cabin_deck_T"], axis=1)
+
 # print(train_X)
 
 train_y = train_raw.Transported
@@ -35,8 +37,8 @@ iter_range = list(range(0,10))
 range_tol = [1e-4, 1e-3, 1e-2, 1e-1, 1]
 range_C = list(range(1,15))
 
-# pgrid = dict(max_iter=iter_range, solver = ["newton-cholesky"], tol = range_tol, C = range_C, fit_intercept = [False, True], class_weight = ["balanced", None])
-pgrid = dict(max_iter=iter_range, solver = ["newton-cholesky"], tol = range_tol, C = range_C, fit_intercept = [False], class_weight = ["balanced"])
+# pgrid = dict(max_iter=iter_range, solver = ["newton-cg"], tol = range_tol, C = range_C, fit_intercept = [False, True], class_weight = ["balanced", None])
+pgrid = dict(max_iter=iter_range, solver = ["newton-cholesky"], tol = range_tol, C = range_C, fit_intercept = [False])
 
 grid = GS_CV(log, pgrid, scoring='accuracy', n_jobs = -1, cv =10, return_train_score=True)
 
